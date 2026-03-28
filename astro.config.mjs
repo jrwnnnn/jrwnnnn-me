bf600292302ab92d409b682b0697c965762e8944
Mark Jerwin <136870415+jrwnnnn@users.noreply.github.com>
2026-03-28 08:48:49 +0800

    Astro bump (#30)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import node from "@astrojs/node";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
